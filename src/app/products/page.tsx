6c0d8702210ff332c9a5a73f35567688b569f4cb
Gustavo Yuji Okagawa <yudi.gustavo@outlook.com>
2025-02-21 22:15:53 -0300

    feat: add shadcn inital setud and components

---
+++ src/app/products/page.tsx
@@ -0,0 +1,9 @@
+const ProductPage = () => {
+  return (
+    <div className="p-5 border-red-500 rounded-xl">
+      <h1 className="text-red-950">hello product!</h1>
+    </div>
+  );
+};
+
+export default ProductPage;
